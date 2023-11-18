05c3521821fe3db77d6dac31e85b46c43aab499f
Maksim Hvatov <uzhas76064@gmail.com>
2023-11-18 07:43:00 +0300

    добавлена валидация формы

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 const navLinks = document.querySelectorAll(".nav__link");
 const mainContentButton = document.querySelector(".main-content__button");
 const productsSection = document.querySelector(".products");
+const orderInputs = document.querySelectorAll(".order-form__input");
+const orderButton = document.querySelector(".order-button");
 
 const scrollToSection = (sectionName) => {
     const section = document.querySelector(`.${sectionName}`);
@@ -18,3 +20,25 @@ navLinks.forEach((navLink) => {
         scrollToSection(link)
     });
 })
+
+orderButton.addEventListener("click", () => {
+    let hasError = false;
+
+    orderInputs.forEach((input) => {
+        if(!input.value) {
+            hasError = true;
+            input.parentElement.style.background = "red";
+        }
+        else input.parentElement.style.background = "";
+    })
+
+    if (!hasError) {
+        orderInputs.forEach((input) => {
+            input.value = "";
+        })
+
+        alert("Спасибо за заказ! Мы скоро свяжемся с вами!");
+    }
+})
+
+
