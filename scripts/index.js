27424c4b7fa91d03df304381b6fd0575058870f5
Maksim Hvatov <uzhas76064@gmail.com>
2023-11-18 08:38:12 +0300

    выполнено задание 4

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ const mainContentButton = document.querySelector(".main-content__button");
 const productsSection = document.querySelector(".products");
 const orderInputs = document.querySelectorAll(".order-form__input");
 const orderButton = document.querySelector(".order-button");
+const currencyButton = document.querySelector(".currency");
+const prices = document.querySelectorAll(".products-item__price");
 
 const scrollToSection = (sectionName) => {
     const section = document.querySelector(`.${sectionName}`);
@@ -41,4 +43,34 @@ orderButton.addEventListener("click", () => {
     }
 })
 
+currencyButton.addEventListener("click", (evt) => {
+    let currentCurrency = evt.target.innerText;
+
+    let newCurrency = "$";
+    let coefficient = 1;
+
+    if (currentCurrency === "$") {
+        newCurrency = "₽";
+        coefficient = 89.35;
+    }
+    else if (currentCurrency === "₽") {
+        newCurrency = "BYN";
+        coefficient = 3.28;
+    }
+    else if (currentCurrency === 'BYN') {
+        newCurrency = '€';
+        coefficient = 0.9;
+    }
+    else if (currentCurrency === '€') {
+        newCurrency = '¥';
+        coefficient = 6.9;
+    }
+
+    evt.target.innerText = newCurrency;
+
+    prices.forEach((price) => {
+        console.log(price.getAttribute("data-price"))
+        price.innerText = (price.getAttribute("data-price") * coefficient).toFixed(1) + newCurrency;
+    })
+})
 
